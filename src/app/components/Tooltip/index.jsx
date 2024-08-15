211f201da273697d8218c69da5b7a7cef08dfdd6
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-08-15 03:53:39 -0600

    Sin Scroll Navigation

--- src/app/components/Tooltip/index.jsx
+++ src/app/components/Tooltip/index.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 const Tooltip = ({ children, toolTipText }) => {
+  const [selected, setSelected] = useState(false);
   return (
-    <div className="tooltip">
+    <div
+      className={`tooltip ${selected ? "scale-105" : ""}`}
+      onClick={() => setSelected((prev) => !prev)}
+    >
       {children}
       <span className="tooltiptext">{toolTipText}</span>
     </div>
